b711e436f398316cb07a75bdbe6eab1ab4ae4c74
Surya pratap sahi <suryasahi900@gmail.com>
2023-11-03 22:28:21 +0530

    Rotate List (LinkedList)

---
+++ 1. Rotate List.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null)return head;
+        //getting length
+        ListNode point = head;
+        int length = 1;
+        while(point.next != null){
+            point = point.next;
+            length++;
+        }
+        k = k % length;
+        if(k == 0)return head;
+        while(k-- > 0){
+            ListNode prev = null;
+            ListNode curr = head;
+            while(curr.next != null){
+                prev = curr;
+                curr = curr.next;
+            }
+            prev.next = null;
+            curr.next = head;
+            head = curr;
+        }
+        return head;
+    }
+}
