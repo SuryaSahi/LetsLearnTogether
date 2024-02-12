56495b658a56dbc3789ecf908f106039525a63c5
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-02-12 21:14:35 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (52.00%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,28 @@
+class Solution {
+    int total;
+    public int myAtoi(String s) {
+        total=0;
+        return myAtoi(s,0,true,false);
+    }
+
+    int myAtoi(String S,int ind,boolean sign,boolean num){
+        if(ind==S.length()) return 0;
+        if(!num){
+            if(S.charAt(ind)==' ') return myAtoi(S,ind+1,sign,num);
+            if(S.charAt(ind)=='-' || S.charAt(ind)=='+'){
+                sign=(S.charAt(ind)=='-'?false:true);
+                return myAtoi(S,ind+1,sign,!num);
+            }
+        }
+        num=true;
+        char ch=S.charAt(ind);
+        if(ch-'0'<0 || ch-'0'>9) return 0;
+        if(total>Integer.MAX_VALUE/10 || total==Integer.MAX_VALUE/10 && ch-'0'>7)
+            return sign?Integer.MAX_VALUE:Integer.MIN_VALUE;
+            
+        total=total*10+ch-'0';
+        int t=myAtoi(S,ind+1,sign,num);
+        if(t==0) return sign?total:-total;
+        else return t;
+    }
+}
