3f992a5075e56f0ece3c32f6a250fb2ffa05f246
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-05-01 10:44:26 +0530

    Time: 94 ms (47.97%), Space: 46.6 MB (56.09%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,39 +1,43 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (!wordList.contains(endWord)) {
-    return 0; // If not in wordList, return 0.
-}
-
-int cnt = 0; 
-Set<String> dict = new HashSet<>(wordList);
-Queue<String> que = new LinkedList<>(); 
-
-que.add(beginWord); 
-
-while (!que.isEmpty()) {
-    cnt++; 
-    int size = que.size();
-    // Loop through the elements in the queue.
-    for (int k = 0; k < size; k++) {
-        String word = que.poll(); 
-        // If the word is the same as the endWord, return the counter.
-        if (word.equals(endWord))
-            return cnt;
-        for (int i = 0; i < word.length(); i++) {
-            // Loop through each character 
-            for (char ch = 'a'; ch <= 'z'; ch++) {
-                char[] arr = word.toCharArray();
-                arr[i] = ch; // Change the character at index i to ch.
-                String midWord = new String(arr);
-                // If the new word is in the dictionary, add it to the queue and remove it from the dictionary.
-                if (dict.contains(midWord)) {
-                    que.add(midWord);
-                    dict.remove(midWord);
+        Queue<Pair> q = new LinkedList<>();
+        
+        q.add(new Pair(beginWord , 1));
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0; i<wordList.size(); i++){
+            set.add(wordList.get(i));
+        }
+        
+        while(!q.isEmpty()){
+            String wr = q.peek().first;
+            int steps = q.peek().second;
+            q.remove();
+            
+            if (wr.equals(endWord) == true) return steps;
+            
+            for(int i = 0; i<wr.length(); i++){
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    char charArray[] = wr.toCharArray();
+                    charArray[i] = ch;
+                    String newword = new String(charArray);
+                    
+                    if(set.contains(newword)){
+                        set.remove(newword);
+                        q.add(new Pair(newword , steps + 1));
+                    }
+                    
                 }
             }
         }
+        return  0;
     }
 }
-    return 0;
+
+class Pair{
+    String first;
+    int second;
+    Pair(String first , int second){
+        this.first = first;
+        this.second = second;
     }
 }
