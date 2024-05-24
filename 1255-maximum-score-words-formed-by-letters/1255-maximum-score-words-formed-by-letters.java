379031d1a297f4218a4b6af6ce8c85e56a6a4187
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-05-25 01:13:04 +0530

    Time: 1 ms (80.58%), Space: 41.1 MB (79.34%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -0,0 +1,75 @@
+class Solution1 {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int[] count = new int[26];
+        int[] letterCount = new int[26];
+        
+        for(char i : letters){
+            count[i - 'a']++;
+        }
+        int ans = 0;
+        backtrack(words ,  score , count , letterCount , ans , 0 , 0);
+        return ans;
+    }
+    
+    private int backtrack(String[] words , int[] score , int[] count , int[] letterCount , int ans , int index , int temp){
+        for(int i = 0; i<26; i++){
+            if(letterCount[i] > count[i])return ans;
+        }
+        ans = Math.max(ans , temp);
+        
+        for(int i = index; i<words.length; i++){
+            for(char c : words[i].toCharArray()){
+                letterCount[c - 'a']++;
+                temp += score[c - 'a'];
+            }
+            backtrack(words , score , count , letterCount , ans , index + 1 , temp);
+        
+            for(char c : words[i].toCharArray()){
+                letterCount[c - 'a']--;
+                temp -= score[c - 'a'];
+            }
+        }
+        
+        
+        return ans;
+    }
+}
+
+public class Solution {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int[] count = new int[26];
+        int[] lettersCount = new int[26];
+        
+        for (char c : letters) {
+            count[c - 'a']++;
+        }
+        
+        int ans = 0;
+        ans = backtracking(words, score, count, lettersCount, 0, 0, ans);
+        return ans;
+    }
+    
+    private int backtracking(String[] words, int[] score, int[] count, int[] lettersCount, int pos, int temp, int ans) {
+        for (int i = 0; i < 26; i++) {
+            if (lettersCount[i] > count[i]) return ans;
+        }
+        
+        ans = Math.max(ans, temp);
+        
+        for (int i = pos; i < words.length; i++) {
+            for (char c : words[i].toCharArray()) {
+                lettersCount[c - 'a']++;
+                temp += score[c - 'a'];
+            }
+            
+            ans = backtracking(words, score, count, lettersCount, i + 1, temp, ans);
+            
+            for (char c : words[i].toCharArray()) {
+                lettersCount[c - 'a']--;
+                temp -= score[c - 'a'];
+            }
+        }
+        
+        return ans;
+    }
+}
