096d2a89955279cb03c6acb1bdc28ea711df410a
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-07-31 23:21:10 -0700

    Time: 10 ms (6.23%), Space: 43.6 MB (5.12%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -7,7 +7,7 @@ class Solution {
             String temp = "";
             temp += str.charAt(11);
             temp += str.charAt(12);   
-            System.out.println(temp);
+            //System.out.println(temp);
             if(Integer.valueOf(temp) > 60)cnt++;
         }
         return cnt;
