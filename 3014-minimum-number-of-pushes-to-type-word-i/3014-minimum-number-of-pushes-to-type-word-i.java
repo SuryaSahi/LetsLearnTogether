9679d01d0aa5d141cd2ca983ed8bdddf3ab7aabd
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-08-06 11:00:09 -0700

    Time: 1 ms (33.63%), Space: 41.6 MB (65.63%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int minimumPushes(String word) {
         int result = 0;
         
@@ -13,5 +13,44 @@ class Solution {
             assignkey++;
         }
         return result;
+    }
+}
+
+class Solution {
+     public int minimumPushes(String word) {
+         int result = 0;
+         int[] freq = new int[26];
+         
+         for(int i = 0; i<word.length(); i++){
+             char c = word.charAt(i);
+             freq[c-'a']++;
+         }
+         
+         Arrays.sort(freq);
+         reverse(freq);
+
+         for(int i = 0; i<26; i++){
+             int time = freq[i];
+             
+             int press = i/8 + 1;
+             
+             int total = press * time;
+             result += total;
+         }
+         return result;
+     }
+    private static void reverse(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        
+        while (left < right) {
+            // Swap elements
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            
+            left++;
+            right--;
+        }
     }
 }
