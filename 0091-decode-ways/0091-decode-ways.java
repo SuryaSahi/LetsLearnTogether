3aa4d532e4dea9b09a5e7ebc2ad0f71f3a8e43e9
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-08-06 23:29:16 -0700

    Time: 2 ms (21.94%), Space: 41.4 MB (83.10%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,22 +1,22 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] res = new int[s.length() + 1];
-        Arrays.fill(res , -1);
-        return helper(s , 0 , res);
-    }
-    
-    public int helper(String s , int index , int[] res){
-        if(index == s.length())return 1;
-        if(res[index] != -1) return res[index];
-        if(s.charAt(index) == '0')return 0;
+        //solution done in top down memoization
+        //do approach in bottom up 1D dp 
+        int[] dp = new int[s.length() + 1];
+        if(s.length() == 0)return 0;
         
-        int ways = helper(s , index + 1 , res);
+        dp[s.length()] = 1;
         
-        if(index + 1 < s.length() && Integer.parseInt(s.substring(index , index + 2 )) <= 26)
-            ways += helper(s , index + 2 , res);
-        
-        res[index] = ways;
-        
-        return ways;
+        for(int i = s.length() - 1; i>= 0; i--){
+            if(s.charAt(i) == '0')dp[i] = 0;
+            
+            else {
+                dp[i] += dp[i + 1];
+            
+            if(i + 1 < s.length() && Integer.parseInt(s.substring(i , i+2)) <= 26)
+                dp[i] += dp[i+2];
+            }
+        }
+        return dp[0];
     }
 }
