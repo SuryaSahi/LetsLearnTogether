acafb2988070d40368841bd061bacf6113f86309
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-08-15 04:33:53 -0700

    Time: 80 ms (47.48%), Space: 46.3 MB (39.85%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public String removeStars(String s) {
         List<Character> lst = new ArrayList<>();
         String res = "";
@@ -11,5 +11,24 @@ class Solution {
             res += lst.get(i);
         }
         return res;
+    }
+}
+
+class Solution {
+    public String removeStars(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c == '*')st.pop();
+            else st.push(c);
+        }
+        
+        StringBuilder res = new StringBuilder();
+        while(!st.isEmpty()){
+            res.append(st.pop());
+        }
+        
+        return res.reverse().toString();
     }
 }
