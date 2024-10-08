a9eea0b77556a051cf223bcc357378a1ac0d8d35
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-10-08 13:57:55 +0530

    Time: 10 ms (75.06%), Space: 41.8 MB (71.91%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    static final int mod = 1000000007;
+    static final int mod = 1000000007; // modulo 
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n + 1][target + 1];
         for (int[] row : dp) {
@@ -10,15 +10,16 @@ class Solution {
     }
     
     public int solve(int n , int k , int target , int[][] dp){
-        if(target < 0)return 0;
-        if((n == 0 && target > 0) || (n != 0 && target <= 0)) return 0;
+        // BASE CASE
+        if((n == 0 && target > 0) || (n != 0 && target <= 0) || target < 0) return 0;
         
         if(n == 0 && target == 0)return 1;
-        if(dp[n][target] != -1)return dp[n][target];
+        if(dp[n][target] != -1)return dp[n][target]; //overlapping sub problem saved values
         int ans = 0;
         for(int i = 1; i<=k; i++){
             ans = (ans + solve(n - 1, k, target - i , dp)) % mod;
         }
+        //memoize the value
         return dp[n][target] = ans;
     }
 }
