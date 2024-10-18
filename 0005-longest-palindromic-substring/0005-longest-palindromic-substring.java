0a496d99b2cf0fa4fefca21db61337dc43b8e3f6
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-10-18 22:44:50 +0530

    Time: 250 ms (26.32%), Space: 56.4 MB (5.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution1 {
+    //O(n^2) solution where we assume a character to be center and then on we check its adjacent
     public String longestPalindrome(String s) {
         String res = "";
         int reslen = 0;
@@ -27,5 +28,37 @@ class Solution {
             }
         }
         return res;
+    }
+}
+
+class Solution{
+    //O(n^2) approach with recursion and memoization
+    public String longestPalindrome(String s){
+        int maxlen = 0;
+        String res = "";
+        int n = s.length();
+        
+        int[][] memotab = new int[n + 1][n + 1];
+        for (int[] row : memotab) Arrays.fill(row, -1);
+        for(int i = 0; i<n; i++){
+            for(int j = i; j<n; j++){
+                if(palindromeChecker(i,j,s,memotab) == 1){
+                    if((j - i + 1) > maxlen){
+                        maxlen = j - i  + 1;
+                        res = s.substring(i,j + 1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    public int palindromeChecker(int i , int j , String str , int[][] memotab){
+        if( i >= j)return 1;
+        
+        if(memotab[i][j] != -1)return memotab[i][j];
+        
+        if(str.charAt(i) == str.charAt(j))return memotab[i][j] = palindromeChecker(i+1,j-1,str,memotab);
+        
+        return memotab[i][j] = 0;
     }
 }
