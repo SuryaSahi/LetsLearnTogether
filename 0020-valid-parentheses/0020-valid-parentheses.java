2be84e85154deec834c5228aa928cff257f1dc3c
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-10-21 12:14:14 +0530

    Time: 2 ms (59.39%), Space: 41.7 MB (28.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(')st.push(')');
+            else if(c == '{')st.push('}');
+            else if(c == '[')st.push(']');
+            else if(st.isEmpty() || st.pop() != c)return false;
+        }
+        return st.isEmpty();
+    }
+}
