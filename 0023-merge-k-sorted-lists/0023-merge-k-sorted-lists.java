f4763100623ebb0d6c05a7f5f649cf8522efe734
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-10-22 11:33:56 +0530

    Time: 5 ms (39.73%), Space: 44.2 MB (79.92%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,7 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution1 {
+    //Time Complexity - O(NlogN)
+    //Space Complexity - O(N)
     public ListNode mergeKLists(ListNode[] lists) {
         ArrayList<Integer> arr = new ArrayList<>();
         for(ListNode node : lists){
@@ -25,6 +27,32 @@ class Solution {
         for(int i : arr){
             ListNode temp = new ListNode(i);
             dummy.next = temp;
+            dummy = dummy.next;
+        }
+        
+        return newnode.next;
+    }
+}
+
+class Solution {
+    //Time Complextiy - O(N log K)
+    //Space Complextiy - O(N)
+    public ListNode mergeKLists(ListNode[] lists){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(ListNode node : lists){
+            while(node != null){
+                pq.offer(node.val);
+                node = node.next;
+            }
+        }
+        
+        if(pq.isEmpty())return null;
+        
+        ListNode newnode = new ListNode(0);
+        ListNode dummy = newnode;
+        
+        while(!pq.isEmpty()){
+            dummy.next = new ListNode(pq.poll());
             dummy = dummy.next;
         }
         
