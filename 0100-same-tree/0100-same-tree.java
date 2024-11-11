2781762412576f06c8a0c41d9a88c9e3a0d2d094
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-11-11 22:54:58 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (83.59%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,3 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution1 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> que1 = new LinkedList<>();
+        Queue<TreeNode> que2 = new LinkedList<>();
+        
+        que1.add(p);
+        que1.add(q);
+        
+        while(!que1.isEmpty() && !que2.isEmpty()){
+            TreeNode temp1 = que1.poll();
+            TreeNode temp2 = que2.poll();
+            
+            if(temp1 == null && temp2 == null)continue;
+            if(temp1 == null || temp2 == null)return false;
+            if(temp1.val != temp2.val)return false;
+            
+            que1.add(temp1.left);
+            que2.add(temp2.left);
+            que1.add(temp1.right);
+            que2.add(temp2.right);
+        }
+        
+        return que1.isEmpty() && que2.isEmpty();
+    }
+}
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         Queue<TreeNode> que1 = new LinkedList<>();
@@ -20,7 +61,7 @@ class Solution {
             que2.add(temp2.right);
         }
         
-        // Correctly check if both queues are empty
+        //check if both queues are empty
         return que1.isEmpty() && que2.isEmpty();
     }
 }
