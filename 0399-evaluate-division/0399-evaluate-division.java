4f1d23231630db706f684fc4bf935fd268476bdb
Surya Pratap Sahi <suryasahi900@gmail.com>
2024-11-28 15:35:08 -0800

    Time: 2 ms (32.11%), Space: 42.3 MB (32.63%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,48 +1,73 @@
 class Solution {
     public double[] calcEquation(List<List<String>> eq, double[] values, List<List<String>> queries) {
-        // Creating a graph (adjacency list)
         Map<String, Map<String, Double>> graph = new HashMap<>();
-        
+
         for (int i = 0; i < eq.size(); i++) {
             String u = eq.get(i).get(0);
             String v = eq.get(i).get(1);
 
             graph.putIfAbsent(u, new HashMap<>());
-            graph.get(u).put(v, values[i]);
-
             graph.putIfAbsent(v, new HashMap<>());
+
+            graph.get(u).put(v, values[i]);
             graph.get(v).put(u, 1 / values[i]);
         }
 
         double[] ans = new double[queries.size()];
+
         for (int i = 0; i < queries.size(); i++) {
-            // HashSet to track visited nodes
-            ans[i] = dfs(graph, queries.get(i).get(0), queries.get(i).get(1), new HashSet<>());
+            String src = queries.get(i).get(0);
+            String dest = queries.get(i).get(1);
+
+            if (!graph.containsKey(src) || !graph.containsKey(dest)) {
+                ans[i] = -1.0;
+            } else if (src.equals(dest)) {
+                ans[i] = 1.0;
+            } else {
+                ans[i] = bfs(graph, src, dest);
+            }
         }
 
-        return ans; 
+        return ans;
     }
 
-    public double dfs(Map<String, Map<String, Double>> graph, String src, 
-                      String dest, HashSet<String> visited) {
-        // If source node doesn't exist in the graph
-        if (!graph.containsKey(src)) return -1.0;
+    private double bfs(Map<String, Map<String, Double>> graph, String src, String dest) {
+        Queue<Pair> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
 
-        // If there is a direct edge to the destination
-        if (graph.get(src).containsKey(dest)) return graph.get(src).get(dest);
-
+        queue.offer(new Pair(src, 1.0));
         visited.add(src);
 
-        for (Map.Entry<String, Double> nbr : graph.get(src).entrySet()) {
-            if (!visited.contains(nbr.getKey())) {
-                double weight = dfs(graph, nbr.getKey(), dest, visited);
+        while (!queue.isEmpty()) {
+            Pair current = queue.poll();
+            String node = current.node;
+            double value = current.value;
 
-                if (weight != -1.0) {
-                    return nbr.getValue() * weight;
+            if (node.equals(dest)) {
+                return value;
+            }
+
+            for (Map.Entry<String, Double> neighbor : graph.get(node).entrySet()) {
+                String nextNode = neighbor.getKey();
+                double edgeWeight = neighbor.getValue();
+
+                if (!visited.contains(nextNode)) {
+                    visited.add(nextNode);
+                    queue.offer(new Pair(nextNode, value * edgeWeight));
                 }
             }
         }
 
         return -1.0;
+    }
+}
+
+class Pair {
+    String node;
+    double value;
+
+    Pair(String node, double value) {
+        this.node = node;
+        this.value = value;
     }
 }
